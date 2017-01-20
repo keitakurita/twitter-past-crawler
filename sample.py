c838776a1140d57bb976bc8c85f5e1f2b35fe374
栗田啓大 <kris337jbn@yahoo.co.jp>
2017-01-20 14:18:34 +0900

    sample

---
+++ sample.py
@@ -0,0 +1,9 @@
+import twittercrawler
+
+crawler = twittercrawler.TwitterCrawler({"query": "@realDonaldTrump",
+                                         "max_depth": 3,
+                                         "parser": twittercrawler.parse_articles,
+                                         "output_file": "DonaldTrump.csv"
+                                         })
+
+crawler.crawl()
